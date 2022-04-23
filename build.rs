2e94f411dc75fe4c048b20d5ec738dae729665a6
Maciej Małecki <me@mmalecki.com>
2022-04-23 17:14:04 +0200

    Migrate to ESP32

--- build.rs
+++ build.rs
@@ -1,16 +1,5 @@
-use std::env;
-use std::fs::File;
-use std::io::Write;
-use std::path::PathBuf;
-fn main() {
-    if env::var_os("CARGO_FEATURE_RT").is_some() {
-        let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
-        File::create(out.join("memory.x"))
-            .unwrap()
-            .write_all(include_bytes!("memory.x"))
-            .unwrap();
-        println!("cargo:rustc-link-search={}", out.display());
-        println!("cargo:rerun-if-changed=memory.x");
-    }
-    println!("cargo:rerun-if-changed=build.rs");
+// Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
+fn main() -> anyhow::Result<()> {
+    embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
+    embuild::build::LinkArgs::output_propagated("ESP_IDF")
 }
---
+++ examples/main.rs
@@ -0,0 +1,33 @@
+use esp_idf_sys as _;
+
+use esp_idf_hal::i2c;
+use esp_idf_hal::peripherals::Peripherals;
+use esp_idf_hal::prelude::*;
+
+use lis331hh::{Lis331hh, SlaveAddr};
+use lis331hh::accelerometer::RawAccelerometer;
+
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    esp_idf_sys::link_patches();
+
+    let peripherals = Peripherals::take().unwrap();
+    let i2c = peripherals.i2c0;
+    let sda = peripherals.pins.gpio5;
+    let scl = peripherals.pins.gpio6;
+
+    println!("Starting LIS331 hello-world");
+
+    let config = <i2c::config::MasterConfig as Default>::default().baudrate(100.kHz().into());
+    let mut i2c = i2c::Master::<i2c::I2C0, _, _>::new(i2c, i2c::MasterPins { sda, scl }, config).unwrap();
+
+    let mut sensor = Lis331hh::new_i2c(i2c, SlaveAddr::Default).unwrap();
+
+    loop {
+        let accel = sensor.accel_raw().unwrap();
+        println!("{:?}", accel);
+        thread::sleep(Duration::from_millis(250));
+    }
+}
